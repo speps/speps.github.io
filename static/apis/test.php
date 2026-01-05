08f47791939903998a0ed16e91d3fdbfd9359155
Remi Gillig <remigillig@gmail.com>
2026-01-05 16:36:29 +0000

    FFI test

--- static/apis/test.php
+++ static/apis/test.php
@@ -16,3 +16,14 @@ if (strlen($input) > 0) {
 }
 ?>
 </pre>
+<pre>
+FFI
+<?php
+// create FFI object, loading libc and exporting function printf()
+$ffi = FFI::cdef(
+    "int printf(const char *format, ...);", // this is a regular C declaration
+    "libc.so.6");
+// call C's printf()
+$ffi->printf("Hello %s!\n", "world");
+?>
+</pre>
